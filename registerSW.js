3e6de6aa08ad66f87d1ceecbba01b5864d571639
jumz96 <jumzbms@gmail.com>
2025-08-24 12:20:34 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://github.com/jumadil94-hub/node-calculator.git/sw.js', { scope: 'https://github.com/jumadil94-hub/node-calculator.git/' })})}
